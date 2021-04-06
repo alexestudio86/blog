b8152db0fedba97dc0e7101bf43cc78b6c632732
alexestudio86 <80129697+alexestudio86@users.noreply.github.com>
2021-04-06 15:15:22 -0500

    Update lightbox.js

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -6,7 +6,7 @@ const lightboxElements = document.querySelectorAll('img.lightbox');
 const lightbox = (e) => {
   extBtn = document.createElement('span');
   extBtn.classList.add('w3-xlarge', 'w3-white', 'w3-button', 'w3-display-topright');
-  extBtn.textContent = '&times;';
+  extBtn.innerHTML  = '&times;';
   extBtn.addEventListener('click', modalGeneral);
   imgContainer = document.createElement('figure');
   imgContainer.classList.add('w3-display-middle', 'w-100',  'w3-content','animate__animated','animate__fadeIn');
@@ -16,8 +16,8 @@ const lightbox = (e) => {
     imgPicture.setAttribute('alt', e.currentTarget.getAttribute('alt'));
     imgPicture.setAttribute('src', e.currentTarget.getAttribute('src'));
     imgFigCaption = document.createElement('figcaption');
+    imgFigCaption.classList.add('w3-black', 'w3-opacity', 'w3-large', 'w3-padding-small' ,'w3-display-bottommiddle', 'w-100', 'w3-center');
     imgFigCaption.textContent = e.target.getAttribute('alt');
-    imgFigCaption.classList.add('w3-black', 'w3-opacity', 'w3-xlarge', 'w3-center');
 
   modal.classList.add('w3-black');
   modal.appendChild(extBtn);
