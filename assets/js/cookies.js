b012bcd48b7e30c65417d4adea3d51771a40b43a
alexestudio86 <80129697+alexestudio86@users.noreply.github.com>
2021-04-05 20:34:29 -0500

    Update cookies.js

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -1,63 +1,44 @@
-// WELCOME MODAL
-const popWindow = localStorage.getItem('popWindow');
-if(!popWindow){
+// COOKIES MODAL
+const cookiesWindow = localStorage.getItem('privacity');
+if(!cookiesWindow){
 
   modalGeneral();
 
   let modal = document.getElementById('modal');
 
     modalContent = document.createElement('div');
-    modalContent.classList.add('w3-modal-content', 'w3-animate-left');
-
-      exitBtn = document.createElement('a');
-      exitBtn.classList.add('w3-white', 'w3-button', 'w3-xlarge', 'w3-display-topright');
-      exitBtn.innerHTML = '&times;';
-      exitBtn.addEventListener('click', modalGeneral);
-      titleContainer = document.createElement('div');
-      titleContainer.classList.add('w3-large', 'w3-padding');
-        titleIconLeft = document.createElement('i');
-        titleIconLeft.classList.add('far', 'fa-star');
-        titleIconLeft.style = 'margin: 0 4px 0 0';
-        titleContent = document.createElement('span');
-        titleContent.textContent = 'Novedades';
-        titleIconRight = document.createElement('i');
-        titleIconRight.classList.add('far', 'fa-star');
-        titleIconRight.style = 'margin: 0 0 0 4px';
-
-      divContainer = document.createElement('div');
-      divContainer.classList.add('w3-container');
-        bulletsContainer = document.createElement('ul');
-        bulletsContainer.classList.add('w3-padding', 'w3-black');
-          firstContent = document.createElement('li');
-          firstContent.classList.add('w3-padding');
-          firstContent.textContent = 'Ahora tu carrito se guarda automáticamente para retomar tu pedido mas tarde';
-          secondContent = document.createElement('li');
-          secondContent.classList.add('w3-padding');
-          secondContent.textContent = 'Ya puedes compartir en tus redes sociales nuestros productos';
-          thirdContent = document.createElement('li');
-          thirdContent.classList.add('w3-padding');
-          thirdContent.textContent = 'Implementamos una barra de búsqueda para que encuentres lo que buscas';
-          fourthContent = document.createElement('li');
-          fourthContent.classList.add('w3-padding');
-          fourthContent.textContent = 'Mejoramos el diseño del sitio optando por un estilo mas limpio';
-          fifthContent = document.createElement('li');
-          fifthContent.classList.add('w3-padding');
-          fifthContent.textContent = 'Optimizamos el sitio para que nos encuentres mas rápido en google';
+    modalContent.classList.add('w3-modal-content', 'w3-round');
+      headerContainer = document.createElement('header');
+      headerContainer.classList.add('w3-container', 'w3-border');
+        titleContainer = document.createElement('h1');
+        titleContainer.classList.add('w3-large', 'w3-padding');
+        titleContainer.textContent = 'Uso de datos';
+      firstMessage = document.createElement('p');
+      firstMessage.classList.add('w3-container');
+      firstMessage.textContent = '<data:messages.euCookieNotice2018/> consulte mas detalles en nestro ';
+        spanMessage = document.createElement('a');
+        spanMessage.classList.add('w3-text-red');
+        spanMessage.setAttribute('href', 'https://www.alexestudio86.com/p/aviso-de-privacidad.html');
+        spanMessage.setAttribute('target', '_blank');
+        spanMessage.textContent = 'Aviso de privacidad';
+      footerContainer = document.createElement('footer');
+      footerContainer.classList.add('w3-row', 'w3-border');
+        btnContainer = document.createElement('div');
+        btnContainer.classList.add('w3-right', 'w3-padding-small','w3-container');
+          acceptBtn = document.createElement('a');
+          acceptBtn.classList.add('w3-button', 'w3-round', 'w3-blue');
+          acceptBtn.textContent = 'Aceptar';
+          acceptBtn.addEventListener('click', modalGeneral);
 
     modal.appendChild(modalContent);
-      modalContent.appendChild(exitBtn);
-      modalContent.appendChild(titleContainer);
-        titleContainer.appendChild(titleIconLeft);
-        titleContainer.appendChild(titleContent);
-        titleContainer.appendChild(titleIconRight);
-      modalContent.appendChild(divContainer);
-        divContainer.appendChild(bulletsContainer);
-          bulletsContainer.appendChild(firstContent);
-          bulletsContainer.appendChild(secondContent);
-          bulletsContainer.appendChild(thirdContent);
-          bulletsContainer.appendChild(fourthContent);
-          bulletsContainer.appendChild(fifthContent);
+      modalContent.appendChild(headerContainer);
+        headerContainer.appendChild(titleContainer);
+      modalContent.appendChild(firstMessage);
+        firstMessage.appendChild(spanMessage);
+      modalContent.appendChild(footerContainer);
+        footerContainer.appendChild(btnContainer);
+          btnContainer.appendChild(acceptBtn);
 
-  localStorage.setItem('popWindow', 'welcome');
+  localStorage.setItem('popWindow', 'privacity');
 
 }
